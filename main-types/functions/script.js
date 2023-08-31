86caf0515470372e24b505bc3678f9597cef5135
Maksym <siroshtan.max1991ua@gmail.com>
2023-08-31 22:34:23 +0300

    Functions

---
+++ main-types/functions/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// Functions
+function getFullName(firstname, surname) {
+    return `${firstname} ${surname}`;
+}
+const getFullNameTwo = (firstname, surname) => {
+    return `${firstname} ${surname}`;
+};
---
+++ main-types/functions/script.ts
@@ -0,0 +1,8 @@
+// Functions
+function getFullName(firstname: string, surname: string): string {
+  return `${firstname} ${surname}`;
+}
+
+const getFullNameTwo = (firstname: string, surname: string): string => {
+  return `${firstname} ${surname}`;
+};
