f1de2cb50a7245a4061064725a7bd22512194c5a
Maksym <siroshtan.max1991ua@gmail.com>
2023-08-31 22:30:10 +0300

    String, number, boolean

---
+++ main-types/primitives/script.js
@@ -0,0 +1,9 @@
+"use strict";
+// String, number, boolean
+const salaryA = 1000;
+// const salaryB: number = 'string'; Type 'string' is not assignable to type 'number'.
+const bonus = 500;
+const bool = true;
+const strA = "string";
+// const strB: string = 100; Type 'number' is not assignable to type 'string'.
+const res = salaryA + bonus;
---
+++ main-types/primitives/script.ts
@@ -0,0 +1,8 @@
+// String, number, boolean
+const salaryA: number = 1000;
+// const salaryB: number = 'string'; Type 'string' is not assignable to type 'number'.
+const bonus: number = 500;
+const bool: boolean = true;
+const strA: string = "string";
+// const strB: string = 100; Type 'number' is not assignable to type 'string'.
+const res: number = salaryA + bonus;
