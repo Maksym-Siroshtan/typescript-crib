eb31e15c92d259ba32c5e057c4a0124066cfc423
Maksym <siroshtan.max1991ua@gmail.com>
2023-09-01 17:11:53 +0300

    Union

---
+++ advanced/union/script.js
@@ -0,0 +1,36 @@
+"use strict";
+function logId(id) {
+    if (typeof id === "string") {
+        console.log(id.toLowerCase()); // string
+    }
+    else if (typeof id === "number") {
+        console.log(id.toFixed(2)); // number
+    }
+    else {
+        console.log(id); // boolean
+    }
+}
+function logErr(err) {
+    if (Array.isArray(err)) {
+        console.log(err); // array
+    }
+    else {
+        console.log(err); // string
+    }
+}
+function logObj(obj) {
+    if ("a" in obj) {
+        console.log(obj.a); // obj.a
+    }
+    else {
+        console.log(obj.b); // obj.b
+    }
+}
+function logAorB(a, b) {
+    if (a === b) {
+        console.log(a); // string
+    }
+    else {
+        console.log(b); // string | boolean
+    }
+}
