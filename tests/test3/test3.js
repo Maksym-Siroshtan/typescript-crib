6e77b85f6debbb837cd57ec7ff328d4629e260c2
Maksym <siroshtan.max1991ua@gmail.com>
2023-09-01 18:05:51 +0300

    Server response typing

---
+++ tests/test3/test3.js
@@ -0,0 +1,27 @@
+"use strict";
+var StatusRequest;
+(function (StatusRequest) {
+    StatusRequest["Success"] = "success";
+    StatusRequest["Failed"] = "failed";
+})(StatusRequest || (StatusRequest = {}));
+const payment = {
+    sum: 10000,
+    from: 2,
+    to: 4,
+};
+const successResponse = {
+    status: StatusRequest.Success,
+    data: {
+        databaseId: 567,
+        sum: 10000,
+        from: 2,
+        to: 4,
+    },
+};
+const failedResponse = {
+    status: StatusRequest.Failed,
+    data: {
+        errorMessage: "Недостаточно средств",
+        errorCode: 4,
+    },
+};
---
+++ tests/test3/test3.ts
@@ -0,0 +1,55 @@
+enum StatusRequest {
+  Success = "success",
+  Failed = "failed",
+}
+
+interface IPayment {
+  sum: number;
+  from: number;
+  to: number;
+}
+
+interface IPaymentRequest extends IPayment {}
+
+interface IPaymentSuccessResponse extends IPayment {
+  databaseId: number;
+}
+
+interface IPaymentFailedRequest {
+  errorMessage: string;
+  errorCode: number;
+}
+
+interface ISuccessResponse {
+  status: StatusRequest.Success;
+  data: IPaymentSuccessResponse;
+}
+
+interface IFailedResponse {
+  status: StatusRequest.Failed;
+  data: IPaymentFailedRequest;
+}
+
+const payment: IPaymentRequest = {
+  sum: 10000,
+  from: 2,
+  to: 4,
+};
+
+const successResponse: ISuccessResponse = {
+  status: StatusRequest.Success,
+  data: {
+    databaseId: 567,
+    sum: 10000,
+    from: 2,
+    to: 4,
+  },
+};
+
+const failedResponse: IFailedResponse = {
+  status: StatusRequest.Failed,
+  data: {
+    errorMessage: "Недостаточно средств",
+    errorCode: 4,
+  },
+};
