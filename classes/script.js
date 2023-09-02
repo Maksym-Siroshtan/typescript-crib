fb013aa1e7e3a8d7a817f969157423f8953afd19
Maksym <siroshtan.max1991ua@gmail.com>
2023-09-02 12:00:49 +0300

    Class constructor

--- classes/script.js
+++ classes/script.js
@@ -8,3 +8,35 @@ const userClass = new User("John");
 console.log(userClass);
 userClass.name = "Alex";
 console.log(userClass);
+// class Admin {
+//   role: number; // Property 'role' has no initializer and is not definitely assigned in the constructor.
+// }
+// class Admin {
+//   role!: number; ! or "strictPropertyInitialization": false
+// }
+class Admin {
+}
+const admin = new Admin();
+admin.role = 1;
+// constructor
+class User2 {
+    constructor(nameOrAge, age) {
+        if (typeof nameOrAge === "string") {
+            this.name = nameOrAge;
+        }
+        else if (typeof nameOrAge === "number") {
+            this.age = nameOrAge;
+        }
+        if (typeof age === "number") {
+            this.age = age;
+        }
+    }
+}
+const userClass2 = new User2();
+console.log("userClass2: ", userClass2);
+const userClass3 = new User2("John");
+console.log("userClass3: ", userClass3);
+const userClass4 = new User2(31);
+console.log("userClass4: ", userClass4);
+const userClass5 = new User2("John", 31);
+console.log("userClass5: ", userClass5);
--- classes/script.ts
+++ classes/script.ts
@@ -25,3 +25,33 @@ class Admin {
 
 const admin = new Admin();
 admin.role = 1;
+
+// constructor
+class User2 {
+  name: string;
+  age: number;
+
+  constructor();
+  constructor(name: string);
+  constructor(age: number);
+  constructor(name: string, age: number);
+  constructor(nameOrAge?: string | number, age?: number) {
+    if (typeof nameOrAge === "string") {
+      this.name = nameOrAge;
+    } else if (typeof nameOrAge === "number") {
+      this.age = nameOrAge;
+    }
+    if (typeof age === "number") {
+      this.age = age;
+    }
+  }
+}
+
+const userClass2 = new User2();
+console.log("userClass2: ", userClass2);
+const userClass3 = new User2("John");
+console.log("userClass3: ", userClass3);
+const userClass4 = new User2(31);
+console.log("userClass4: ", userClass4);
+const userClass5 = new User2("John", 31);
+console.log("userClass5: ", userClass5);
