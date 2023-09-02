392bda8d81d7ac88ef7afaf4602ed3fb5489f02f
Maksym <siroshtan.max1991ua@gmail.com>
2023-09-02 12:21:57 +0300

    Creation class

--- classes/script.js
+++ classes/script.js
@@ -1,23 +1,4 @@
 "use strict";
-class User {
-    constructor(name) {
-        this.name = name;
-    }
-}
-const userClass = new User("John");
-console.log(userClass);
-userClass.name = "Alex";
-console.log(userClass);
-// class Admin {
-//   role: number; // Property 'role' has no initializer and is not definitely assigned in the constructor.
-// }
-// class Admin {
-//   role!: number; ! or "strictPropertyInitialization": false
-// }
-class Admin {
-}
-const admin = new Admin();
-admin.role = 1;
 // constructor
 class User2 {
     constructor(nameOrAge, age) {
--- classes/script.ts
+++ classes/script.ts
@@ -1,31 +1,3 @@
-class User {
-  name: string;
-
-  constructor(name: string) {
-    this.name = name;
-  }
-}
-
-const userClass = new User("John");
-console.log(userClass);
-userClass.name = "Alex";
-console.log(userClass);
-
-// class Admin {
-//   role: number; // Property 'role' has no initializer and is not definitely assigned in the constructor.
-// }
-
-// class Admin {
-//   role!: number; ! or "strictPropertyInitialization": false
-// }
-
-class Admin {
-  role: number;
-}
-
-const admin = new Admin();
-admin.role = 1;
-
 // constructor
 class User2 {
   name: string;
