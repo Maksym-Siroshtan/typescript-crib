d1adfde1c8b273fc65b714475f047e50eaf08e2f
Maksym <siroshtan.max1991ua@gmail.com>
2023-09-02 20:30:29 +0300

    Features of inheritance

--- classes/extends/script.js
+++ classes/extends/script.js
@@ -10,7 +10,7 @@ class Payment2 {
 }
 class PersistedPayment extends Payment2 {
     constructor() {
-        const id = Math.random();
+        const id = Math.random(); // Работает, потому что нет инициализации свойств, аргументов и не обращаемся к this. В других случаях super() всегда на первой строке в constructor()
         super(id); // Если переопределяем конструктор обязателен метод супер
     }
     save() {
@@ -22,5 +22,47 @@ class PersistedPayment extends Payment2 {
         if (date) {
             this.paidAt = date;
         }
+    }
+}
+// Особенности наследования
+class User5 {
+    constructor() {
+        this.name = "user";
+        console.log(this.name);
+    }
+}
+// class Admin5 extends User5 {
+//   name: string = "admin";
+//   constructor() {
+// Error!!! Вызов "super" должен быть первой инструкцией в конструкторе, ссылающейся на "super" или "this", если производный класс содержит инициализированные свойства, свойства параметров или частные идентификаторы.
+//     console.log(this.name);
+//     super();
+//   }
+// }
+// class Admin5 extends User5 {
+//   name: string; // Уберём инициализацию свойства и получим другую ошибку
+//   constructor() {
+//     console.log(this.name); //Error!!! super следует вызывать перед получением доступа к this в конструкторе производного класса.
+//     super();
+//   }
+// }
+// class Admin5 extends User5 {
+//   name: string;
+//   constructor() {
+//     console.log("this.name"); // Теперь всё работает, потому что нет инициализации свойства, аргументов и не обращаемся к this
+//     super();
+//   }
+// }
+class Admin5 extends User5 {
+    constructor() {
+        super();
+        console.log(this.name); // Всё корректно, потому что super() на первой строке constructor()
+    }
+}
+// new Error('') Наследуем от встроенного класса
+class HttpError extends Error {
+    constructor(message, code) {
+        super(message);
+        this.code = code !== null && code !== void 0 ? code : 500;
     }
 }
--- classes/extends/script.ts
+++ classes/extends/script.ts
@@ -19,7 +19,7 @@ class PersistedPayment extends Payment2 {
   paidAt: Date;
 
   constructor() {
-    const id = Math.random();
+    const id = Math.random(); // Работает, потому что нет инициализации свойств, аргументов и не обращаемся к this. В других случаях super() всегда на первой строке в constructor()
     super(id); // Если переопределяем конструктор обязателен метод супер
   }
 
@@ -33,5 +33,61 @@ class PersistedPayment extends Payment2 {
     if (date) {
       this.paidAt = date;
     }
+  }
+}
+
+// Особенности наследования
+class User5 {
+  name: string = "user";
+
+  constructor() {
+    console.log(this.name);
+  }
+}
+
+// class Admin5 extends User5 {
+//   name: string = "admin";
+
+//   constructor() {
+// Error!!! Вызов "super" должен быть первой инструкцией в конструкторе, ссылающейся на "super" или "this", если производный класс содержит инициализированные свойства, свойства параметров или частные идентификаторы.
+//     console.log(this.name);
+//     super();
+//   }
+// }
+
+// class Admin5 extends User5 {
+//   name: string; // Уберём инициализацию свойства и получим другую ошибку
+
+//   constructor() {
+//     console.log(this.name); //Error!!! super следует вызывать перед получением доступа к this в конструкторе производного класса.
+//     super();
+//   }
+// }
+
+// class Admin5 extends User5 {
+//   name: string;
+
+//   constructor() {
+//     console.log("this.name"); // Теперь всё работает, потому что нет инициализации свойства, аргументов и не обращаемся к this
+//     super();
+//   }
+// }
+
+class Admin5 extends User5 {
+  name: string;
+
+  constructor() {
+    super();
+    console.log(this.name); // Всё корректно, потому что super() на первой строке constructor()
+  }
+}
+
+// new Error('') Наследуем от встроенного класса
+class HttpError extends Error {
+  code: number;
+
+  constructor(message: string, code?: number) {
+    super(message);
+    this.code = code ?? 500;
   }
 }
