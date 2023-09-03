f49b8941f7567e5bbf75d5efd73b46726a82d3ec
Maksym <siroshtan.max1991ua@gmail.com>
2023-09-03 21:01:22 +0300

    Work with this

---
+++ classes/this/script.js
@@ -0,0 +1,50 @@
+"use strict";
+class Payment4 {
+    constructor() {
+        // this в момент вызова getDate Payment4
+        this.date = new Date();
+    }
+    getDate() {
+        return this.date;
+    }
+}
+const payment4 = new Payment4();
+const user4 = {
+    // this в момент вызова getDate user4
+    id: 1,
+    paymentDate: payment4.getDate,
+};
+console.log("Результат получения на экземпляре класса: ", payment4.getDate()); // Working...
+console.log("Результат получения на объекте юзер. Контекст потерян: ", user4.paymentDate()); // Undefined - контекст потерян, но TS не подсказывает
+// Исправляем поведение this
+class Payment5 {
+    constructor() {
+        this.date = new Date();
+        this.getDateArrow = () => {
+            // Решение 2 Стрелочная функция, т.к. она не создаёт дополнительный контекст
+            return this.date;
+        };
+    }
+    getDate() {
+        // Решение 1.1) Явно указали что this для метода будет Payment, теперь TS подскажет, как потеряем контекст
+        return this.date;
+    }
+}
+const payment5 = new Payment5();
+const user5 = {
+    id: 1,
+    paymentDate: payment5.getDate.bind(payment5),
+    paymentDateArrow: payment5.getDateArrow, // Решение 2 Стрелочная функция
+};
+console.log("Результат решения 1 - bind: ", user5.paymentDate()); // Working...
+console.log("Результат решения 2 - arrow function: ", user5.paymentDateArrow()); // Working...
+// Контекст при наследовании классов
+class PaymentPersistent extends Payment5 {
+    save() {
+        // return super.getDate(); // Working... Обычную функцию получаем через super
+        // return super.getDateArrow(); // Error... TypeError: (intermediate value).getDateArrow is not a function. Т.к. стрелочной функции нет в прототипе объекта super. Поэтому необходимо обращаться с помощью this.getDateArrow()
+        return this.getDateArrow(); // Working... Стрелочную функцию получаем через this
+    }
+}
+const paymentPersistent = new PaymentPersistent();
+console.log("Результат получения на наследуемом экземпляре класса: ", paymentPersistent.save());
