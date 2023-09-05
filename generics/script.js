e3eee1966c68f11a15042cbb3475b98df3eaa910
Maksym <siroshtan.max1991ua@gmail.com>
2023-09-05 20:05:22 +0300

    Function with generic

--- generics/script.js
+++ generics/script.js
@@ -8,6 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+// Built-in generic
 const numbersGenericArray = [1, 2, 3, 4, 5];
 function getData() {
     return __awaiter(this, void 0, void 0, function* () {
@@ -23,3 +24,21 @@ const ckeck = {
     //string:boolean
     kpp: false,
 };
+// Function with generic
+function logMiddleware(data) {
+    console.log(data);
+    return data;
+}
+// const logA = logMiddleware('A');
+// const logOne = logMiddleware(1);
+// Можно явно задать получаемое значение
+const logA = logMiddleware("A");
+const logOne = logMiddleware(1);
+function getSlicedHalf(data) {
+    const l = data.length / 2;
+    return data.slice(0, l);
+}
+// const spliceStringArray = ["a", "b", "c", "d"];
+// const spliceNumberArray = [1, 2, 3, 4];
+const slicedStringArray = getSlicedHalf(["a", "b", "c", "d"]);
+const slicedNumberArray = getSlicedHalf([1, 2, 3, 4]);
