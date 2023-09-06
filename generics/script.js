6a2389d21e100d1143bd24234c03e7d60d93caeb
Maksym <siroshtan.max1991ua@gmail.com>
2023-09-06 19:50:52 +0300

    Limitation generic

--- generics/script.js
+++ generics/script.js
@@ -50,3 +50,25 @@ const logLine = {
         a: 1,
     },
 };
+// Limitation generic
+class Vehicle2 {
+}
+// function kmToMiles<T>(vehicle: T): T {
+//   vehicle.run = vehicle.run * 0.62; // Свойство "run" не существует в типе "T".
+//   return vehicle;
+// }
+function kmToMiles(vehicle) {
+    vehicle.run = vehicle.run * 0.62;
+    return vehicle;
+}
+class LCV extends Vehicle2 {
+}
+//kmToMiles({a: 1}) // Аргумент типа "{ a: number; }" нельзя назначить параметру типа "Vehicle2".
+kmToMiles(new Vehicle2()); // Working...
+kmToMiles(new LCV()); // Working...
+kmToMiles({ run: 1 }); // Working... Объект не обязптельно должен быть инстансом класса
+function logIdAndAdditionalId(id, additionalId) {
+    console.log(id);
+    console.log(additionalId);
+    return { id, data: additionalId };
+}
--- generics/script.ts
+++ generics/script.ts
@@ -56,3 +56,36 @@ const logLine: LogLineType<{ a: number }> = {
     a: 1,
   },
 };
+
+// Limitation generic
+class Vehicle2 {
+  run: number;
+}
+
+// function kmToMiles<T>(vehicle: T): T {
+//   vehicle.run = vehicle.run * 0.62; // Свойство "run" не существует в типе "T".
+//   return vehicle;
+// }
+
+function kmToMiles<T extends Vehicle2>(vehicle: T): T {
+  vehicle.run = vehicle.run * 0.62;
+  return vehicle;
+}
+
+class LCV extends Vehicle2 {
+  capacity: number;
+}
+
+//kmToMiles({a: 1}) // Аргумент типа "{ a: number; }" нельзя назначить параметру типа "Vehicle2".
+kmToMiles(new Vehicle2()); // Working...
+kmToMiles(new LCV()); // Working...
+kmToMiles({ run: 1 }); // Working... Объект не обязптельно должен быть инстансом класса
+
+function logIdAndAdditionalId<T extends string | number, Y>(
+  id: T,
+  additionalId: Y
+): { id: T; data: Y } {
+  console.log(id);
+  console.log(additionalId);
+  return { id, data: additionalId };
+}
