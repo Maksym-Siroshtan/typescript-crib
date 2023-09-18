bc6f525388034323fdd61d7eeef9b63dd2f09e0b
Maksym <siroshtan.max1991ua@gmail.com>
2023-09-18 18:54:43 +0300

    Prototype

---
+++ patterns/generative-patterns/prototype/script.js
@@ -0,0 +1,18 @@
+"use strict";
+class UserHistory {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.createdAt = new Date();
+    }
+    clone() {
+        let target = new UserHistory(this.name, this.email);
+        return target;
+    }
+}
+let userHistory = new UserHistory("John", "j@j.com");
+console.log(userHistory);
+let userHistoryClone = userHistory.clone();
+userHistoryClone.email = "j-clone@j.com";
+console.log(userHistoryClone);
+console.log(userHistory);
---
+++ patterns/generative-patterns/prototype/script.ts
@@ -0,0 +1,24 @@
+interface Prototype<T> {
+  clone(): T;
+}
+
+class UserHistory implements Prototype<UserHistory> {
+  createdAt: Date;
+
+  constructor(public name: string, public email: string) {
+    this.createdAt = new Date();
+  }
+
+  clone(): UserHistory {
+    let target = new UserHistory(this.name, this.email);
+
+    return target;
+  }
+}
+
+let userHistory = new UserHistory("John", "j@j.com");
+console.log(userHistory);
+let userHistoryClone = userHistory.clone();
+userHistoryClone.email = "j-clone@j.com";
+console.log(userHistoryClone);
+console.log(userHistory);
